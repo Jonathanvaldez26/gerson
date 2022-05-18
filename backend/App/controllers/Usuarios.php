49d238641a9c23231f074c0626031e7cbbfa81bb
Gerson26 <gvelassco@gmail.com>
2022-05-18 18:47:48 -0500

    constnacias

--- backend/App/controllers/Usuarios.php
+++ backend/App/controllers/Usuarios.php
@@ -1556,11 +1556,11 @@ html;
         //$num_linea =utf8_decode("Línea: 39");
         //$num_linea2 =utf8_decode("Línea: 39");
 
-        $pdf->SetXY(50, 87);
+        $pdf->SetXY(40, 87);
         $pdf->SetFont('Arial', 'B', 30);
         #4D9A9B
         $pdf->SetTextColor(0, 0, 0);
-        $pdf->Multicell(200, 10, utf8_decode($nombre_completo), 0, 'C');
+        $pdf->Multicell(220, 10, utf8_decode($nombre_completo), 0, 'C');
         $pdf->Output();
         // $pdf->Output('F','constancias/'.$clave.$id_curso.'.pdf');
 
@@ -1596,11 +1596,11 @@ html;
         //$num_linea =utf8_decode("Línea: 39");
         //$num_linea2 =utf8_decode("Línea: 39");
 
-        $pdf->SetXY(50, 87);
+        $pdf->SetXY(40, 87);
         $pdf->SetFont('Arial', 'B', 30);
         #4D9A9B
         $pdf->SetTextColor(0, 0, 0);
-        $pdf->Multicell(200, 10, utf8_decode($nombre_completo), 0, 'C');
+        $pdf->Multicell(220, 10, utf8_decode($nombre_completo), 0, 'C');
         $pdf->Output();
         // $pdf->Output('F','constancias/'.$clave.$id_curso.'.pdf');
 
