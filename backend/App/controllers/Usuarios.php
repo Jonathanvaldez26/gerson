27f644d562d70060472b220a8998c660bdefa9e7
Gerson26 <gvelassco@gmail.com>
2022-05-20 09:10:48 -0500

    insercion en tabla impresion constancia

--- backend/App/controllers/Usuarios.php
+++ backend/App/controllers/Usuarios.php
@@ -1577,18 +1577,17 @@ html;
 
     public function abrirConstanciaP($clave, $id_curso = null)
     {
-        date_default_timezone_set("America/Mexico_City");        
-
-     
+        date_default_timezone_set("America/Mexico_City");   
+             
         // $this->generaterQr($clave_ticket);
         $datos_user = UsuariosDao::getUserRegisterByClave($clave)[0];
         
-        // $data = new \stdClass();            
-        // $data->_id_registrado = $datos_user['id_registrado'];
-        // $data->_tipo_constancia = "Constancia_Presencial";
-        // $data->_fecha_descarga = date("Y-m-d h:i:s");
+        $data = new \stdClass();            
+        $data->_id_registrado = $datos_user['id_registrado'];
+        $data->_tipo_constancia = "Constancia Presencial";
+        $data->_fecha_descarga = date("Y-m-d h:i:s");
  
-        // $id = UsuariosDao::insertConstanciaFechaDescarga($data);
+        $id = UsuariosDao::insertConstanciaFechaDescarga($data);
 
         // $nombre = explode(" ", $datos_user['nombre']);
 
